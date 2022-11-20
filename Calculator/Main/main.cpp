37f1fc618cdd6825d894edf18cda3a7df268da72
Saif Mahmud <saifmahmud366@gmail.com>
2022-11-20 14:18:57 +0600

    Main code

---
+++ Calculator/Main/main.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+class first
+{
+    public:
+    char in,p,t,s,r;
+    int area,a,b,h;
+};
+class second: public first
+{
+    public:
+    void input()
+    {
+        cout<<"For measuring Area, press\n 'P' for Trapezoid\n 'T' for Triangle\n 'S' for Square\n 'R' for Rectangle\n=> ";
+        cin>>in;
+    }
+    void process()
+    {
+        if(in=='p' || in=='P')//trapezoid
+        {
+            cout<<"Trapezoid Area Calculation";
+            cout<<"\nEnter 1st Base Length= ";
+            cin>>a;
+            cout<<"Enter 2nd Base Lenght= ";
+            cin>>b;
+            cout<<"Enter Height= ";
+            cin>>h;
+            area=0.5*(a+b)*h;
+        }
+        else if(in=='t' || in=='T')//triangle
+        {
+            cout<<"Triangle Area Calculation";
+            cout<<"\nEnter Base= ";
+            cin>>b;
+            cout<<"Enter Height= ";
+            cin>>h;
+            area=0.5*b*h;
+        }
+        else if(in=='s' || in=='S')//squarre
+        {
+            cout<<"Square Area Calculation";
+            cout<<"\nEnter Length of a side= ";
+            cin>>a;
+            area=pow(a,2);
+        }
+        else if(in=='r' || in=='R')//Rectangle
+        {
+            cout<<"Rectangle Area Calculation";
+            cout<<"\nEnter Width= ";
+            cin>>a;
+            cout<<"Enter Height= ";
+            cin>>h;
+            area=a*h;
+        }
+        else
+        {
+            cout<<"Sorry! Wrong Input.";
+        }
+        cout<<"Area= "<<area;
+    }
+};
+main()
+{
+    second obj;
+    obj.input();
+    obj.process();
+}
